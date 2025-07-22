9adff2d1fb6b85a7878cdd20fa53ecbcc2d5a867
KarthikChilumula685 <karthikbunny2005@gmail.com>
2025-07-22 19:23:17 +0530

    fix: refactor middleware imports and streamline code structure

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
-import { createMiddleware } from "@arcjet/next";
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import arcjet, { detectBot, shield } from "arcjet";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
@@ -11,9 +11,7 @@ const isProtectedRoute = createRouteMatcher([
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
   rules: [
-    shield({
-      mode: "LIVE",
-    }),
+    shield({ mode: "LIVE" }),
     detectBot({
       mode: "LIVE",
       allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
@@ -21,22 +19,15 @@ const aj = arcjet({
   ],
 });
 
-const clerk =  clerkMiddleware(async (auth, req) => {
+const clerk = clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
 
   if (!userId && isProtectedRoute(req)) {
     const { redirectToSignIn } = await auth();
     return redirectToSignIn();
   }
+
+  return NextResponse.next();
 });
 
-export default createMiddleware(aj,clerk)
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
-};
+export default createMiddleware(aj, clerk);
